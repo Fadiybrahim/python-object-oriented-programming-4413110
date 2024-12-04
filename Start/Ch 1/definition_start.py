75924a731d0007544cdf5bf8da621ffb687f1c3c
Fadi_777 <33900318+Fadiybrahim@users.noreply.github.com>
2024-12-04 13:48:16 +0100

    asd

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,16 @@
 
 
 # TODO: create a basic class
-
+class BOOKS:
+  def __init__(self,title):
+    self.title=title
 
 # TODO: create instances of the class
+book1 = ("Ai 2024")
+book2 = ("no Work world")
 
+# TODO: Use the static method to access a singleton object
+print(book1)
+print(BOOK.title)
 
 # TODO: print the class and property
